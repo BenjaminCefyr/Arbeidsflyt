75c5e1ce8eae2435ef914ac2f816711fb7e2bcfb
Benjamin Ulvevadet <benjamin.ulvevadet@cefyr.no>
2026-09-23 18:40:17 +0200

    Fiks testen: 12 % av 250 gir 30 i mva

--- tests/mva.test.js
+++ tests/mva.test.js
@@ -38,7 +38,6 @@ test("beregner fra brutto: avviser negative beløp", () => {
   assert.throws(() => beregnFraBrutto(-5, SATSER.lav), { message: "Ugyldig beløp" });
 });
 
-// Bevisst feil for å teste at rød CI blokkerer merge. Riktig mva er 25.
-test("TEST AV RØD CI: 25 % av 100 gir 30 i mva", () => {
-  assert.equal(beregnMva(100, SATSER.standard).mva, 30);
+test("12 % av 250 gir 30 i mva", () => {
+  assert.deepEqual(beregnMva(250, SATSER.lav), { netto: 250, mva: 30, brutto: 280 });
 });
